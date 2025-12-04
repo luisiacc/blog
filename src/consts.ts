fdcdc6b55f75fff6ac69af012e8eb77e8b4adace
luisiacc <luiscorderoacc@gmail.com>
2025-12-04 00:37:13 -0600

    logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,5 @@
-export const SITE_TITLE = 'blog';
-export const SITE_DESCRIPTION = 'thoughts on code, systems, and the occasional rant about semicolons.';
+export const SITE_TITLE = "Luis Carlos Cordero's Blog";
+export const SITE_DESCRIPTION =
+  "thoughts on code, systems, and the occasional rant about semicolons.";
+export const SITE_AUTHOR = "Luis Carlos Cordero";
+export const SITE_TAGLINE = "Software Founder & Engineer";
